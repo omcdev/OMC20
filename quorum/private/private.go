e770b96efeb108b2bc3d19c430d6fe3cae387236
Lyon.Nee <lyon.nee@outlook.com>
2020-07-31 10:56:39 +0800

    upload source code

---
+++ quorum/private/private.go
@@ -0,0 +1,30 @@
+package private
+
+import (
+	"os"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/private/engine/notinuse"
+
+	"github.com/ethereum/go-ethereum/private/privatetransactionmanager"
+)
+
+type PrivateTransactionManager interface {
+	Send(data []byte, from string, to []string) ([]byte, error)
+	StoreRaw(data []byte, from string) ([]byte, error)
+	SendSignedTx(data []byte, to []string) ([]byte, error)
+	Receive(data []byte) ([]byte, error)
+}
+
+func FromEnvironmentOrNil(name string) PrivateTransactionManager {
+	cfgPath := os.Getenv(name)
+	if cfgPath == "" {
+		return nil
+	}
+	if strings.EqualFold(cfgPath, "ignore") {
+		return &notinuse.PrivateTransactionManager{}
+	}
+	return privatetransactionmanager.MustNew(cfgPath)
+}
+
+var P = FromEnvironmentOrNil("PRIVATE_CONFIG")
